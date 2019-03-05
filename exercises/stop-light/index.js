bc5cab5586e609d98d30ae79250065e690fe082a
qassabra <46870454+qassabra@users.noreply.github.com>
2019-03-05 18:38:44 +0300

    HW

--- exercises/stop-light/index.js
+++ exercises/stop-light/index.js
@@ -1,23 +1,44 @@
 function turnOffLights() {
     // change background color of stop light to red
+
+let offLights=$(".bulb");
+offLights.css("background","black");
 }
-// turn on stop function 
+// turn on stop function
 function turnOnStopLight() {
     // change background color of stop light to red
+    turnOffLights();
+let stopLight = $("#stopLight");
+stopLight.css("background","red");
+
 }
 
 // turn on slow function
-function turnOnSlowLight() { 
+function turnOnSlowLight() {
 // change background color of slow light to yellow
+turnOffLights();
+let slowLight = $("#slowLight");
+slowLight.css("background","yellow")
 }
 
 // turn on go function
 function turnOnGoLight() {
     // change background color of go light to green
+turnOffLights();
+let goLight=$("#goLight");
+goLight.css("background","green");
+
 }
 
 // set stop light click event
+$("#stopButton").on("click",turnOnStopLight);
 
 // set slow light click event
+$("#slowButton").on("click",turnOnSlowLight);
 
 // set go light click event
+$("#goButton").on("click",turnOnGoLight);
+
+//turn of lights
+
+$("#offLights")
--- exercises/theme-switcher/index.js
+++ exercises/theme-switcher/index.js
@@ -1,13 +1,21 @@
 // click event for gray button
-
+$("#grayButton").on("click",switchGray);
 // click event for white button
+$("#whiteButton").on("click",switchWhite);
 
 function switchGray() {
+  let backgroundGray=$("body");
+backgroundGray.css("background", "gray");
     // change background to gray
+    backgroundGray.css("color", "white")
+
     // change font to white
 }
 
 function switchWhite() {
+  let backgroundWhite=$("body");
+backgroundWhite.css("background", "white");
+backgroundWhite.css("color", "gray")
     // change background to white
     // change font to black
 }
